971a0d231fb422083118df45837f1539ac518d0d
JEEBIS27 <umadashi27@gmail.com>
2026-01-12 13:54:58 +0900

    共通設定をまとめて定義するように変更

---
+++ keyboards/jeebis/mejiro31/rules.mk
@@ -0,0 +1,8 @@
+STENO_ENABLE = yes
+TAPPING_ENABLE = yes
+STRING_ENABLE = yes
+OS_DETECTION_ENABLE = yes
+COMBO_ENABLE = no
+
+SRC += $(KEYBOARD_PATH_1)/combo_fifo.c
+SRC += $(KEYBOARD_PATH_1)/alt_layout.c
