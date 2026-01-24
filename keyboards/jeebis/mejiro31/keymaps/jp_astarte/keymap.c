67aba364fadb752d415da1f9317abf3bb985f474
JEEBIS27 <umadashi46@outlook.jp>
2026-01-24 21:23:37 +0900

    alternative layoutの言語設定を修正

--- keyboards/jeebis/mejiro31/keymaps/jp_astarte/keymap.c
+++ keyboards/jeebis/mejiro31/keymaps/jp_astarte/keymap.c
@@ -67,7 +67,7 @@ static toggle_hold_state_t tg_mjr_state = {false, 0};
 // 0:未使用, 1:英語, 2:日本語, 3:無変更
 static int stn_lang = 2; // ステノ時の言語
 static int kbd_lang = 1; // キーボード時の言語
-static int alt_lang = 1; // Alternative Layoutの言語設定
+static int alt_lang = 3; // Alternative Layoutの言語設定
 
 
 typedef union {
